1704b5f56c600ebda31a9e717a87f85235c387be
Ryan Wedig <ryan@wedig.com>
2017-04-06 22:53:18 -0600

    improving error states and handling live/production database link

--- app.js
+++ app.js
@@ -50,16 +50,20 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = process.NODE_ENV != 'production' ? err : {};
+  res.locals.error = err;
 
   //render the error page
   res.status(err.status || 500);
-  res.render('error');
+  if (process.env.NODE_ENV != 'production') {
+      res.render('error');
+  } else {
+      res.render('error_live');
+  }
 });
 
 // development error handler
 // will print stacktrace
-if (process.NODE_ENV != 'production') {
+if (process.env.NODE_ENV != 'production') {
   app.use(function(err, req, res, next) {
     res.status( err.code || 500 )
     .json({
--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
+require('dotenv').config()
 
 var db
 module.exports.db = function() {
     if (db == null) {
       var pgp = require('pg-promise')();
-//      var connectionString = 'postgres://localhost:5432/puppies';
-	 var connectionString = 'postgres://iaxxikyhixtwny:d5ca15b3169e47472ea96a585e64b7b1f591dab0a8cfbcd24a4d23f2414f6d98@ec2-54-235-119-27.compute-1.amazonaws.com:5432/d2dmunbjf7e31h';
+	  var connectionString = process.env.DATABASE_URL;
       db = pgp(connectionString);
     }
     return db
